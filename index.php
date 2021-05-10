cb8d76344aea8f0d535ab16cecc18c59cbaf06d2
Madmanmayson <dylanhardy97@gmail.com>
2021-05-09 23:06:53 -0400

    Add Personal Info Signup Page

--- index.php
+++ index.php
@@ -9,10 +9,32 @@ require_once 'vendor/autoload.php';
 //Instantiate Fat-Free
 $core = Base::instance();
 
+session_start();
+
 //Define Routes
 $core->route('GET /', function(){
     $view = new Template();
     echo $view->render('views/home.html');
 });
+
+$core->route('GET|POST /signup1', function(){
+
+    //If the form has been submitted, add the data to session
+    //and send the user to the next order form
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $_SESSION['fname'] = $_POST['fname'];
+        $_SESSION['lname'] = $_POST['lname'];
+        $_SESSION['age'] = $_POST['age'];
+        $_SESSION['gender'] = $_POST['gender'];
+        $_SESSION['phone'] = $_POST['phone'];
+        header('location: signup2');
+    }
+
+
+    $view = new Template();
+    echo $view->render('views/personalInfo.html');
+});
+
+
 
 $core->run();
