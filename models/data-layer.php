4970d5cba045b17ba6467a0e2ba7c1bc1de11d27
Madmanmayson <dylanhardy97@gmail.com>
2021-05-20 16:30:09 -0400

    Add validation class for form data

---
+++ models/data-layer.php
@@ -0,0 +1,34 @@
+<?php
+
+class DataLayer
+{
+    /**
+     * @return string[] a array of all possible indoor interests
+     */
+    public static function getIndoorInterests(): array
+    {
+        return array(
+            "tv",
+            "movies",
+            "cooking",
+            "board-games",
+            "puzzles",
+            "reading",
+            "cards",
+            "video-games");
+    }
+
+    /**
+     * @return string[] a array of all possible outdoor interests
+     */
+    public static function getOutdoorInterests(): array
+    {
+        return array(
+            "hiking",
+            "biking",
+            "swimming",
+            "collecting",
+            "walking",
+            "climbing");
+    }
+}
---
+++ models/validation.php
@@ -0,0 +1,65 @@
+<?php
+
+class Validation
+{
+    /**
+     * @param string $name the name to validate
+     * @return bool returns true if valid, otherwise false
+     */
+    public static function validName(string $name): bool
+    {
+        return preg_match("/^[a-zA-Z]+ [a-zA-Z]+$/", $name);
+    }
+
+    /**
+     * @param integer $age the age to validate
+     * @return bool returns true if valid, otherwise false
+     */
+    public static function validAge(int $age): bool
+    {
+        return ($age >= 18 && $age <= 118);
+    }
+
+    /**
+     * @param string $phone the  number to validate
+     * @return bool returns true if valid, otherwise false
+     */
+    public static function validPhone(string $phone): bool
+    {
+        return preg_match("/^\(?[0-9]{3}\)?[ .-]?[0-9]{3}[ .-]?[0-9]{4}$/", $phone);
+    }
+
+    /**
+     * @param array $outdoor the outdoor interests to validate
+     * @return bool returns true if valid, otherwise false
+     */
+    public static function validOutdoor(array $outdoor): bool
+    {
+        $options = DataLayer::getOutdoorInterests();
+        foreach ($outdoor as $interest)
+        {
+            if(!in_array($interest, $options)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param array $indoor the indoor interests to validate
+     * @return bool returns true if valid, otherwise false
+     */
+    public static function validIndoor(array $indoor): bool
+    {
+        $options = DataLayer::getIndoorInterests();
+        foreach ($indoor as $interest)
+        {
+            if(!in_array($interest, $options)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
